9f7e4d6974434f4090240bbc208d1180fbb149e1
Viviana Fu <viviana_fu@wistron.com>
2026-09-18 16:44:41 +0800

    feat: add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/portfolio",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   skipTrailingSlashRedirect: true,
--- src/app/page.page.tsx
+++ src/app/page.page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start ">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/portfolio/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/portfolio/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
